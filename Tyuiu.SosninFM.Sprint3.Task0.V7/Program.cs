4fc7cc2d59ae11de1793ff94980ad298f87696f2
федор соснин <sosninfedor@gmail.com>
2024-10-21 19:36:21 +0500

    Task0

--- Tyuiu.SosninFM.Sprint3.Task0.V7/Program.cs
+++ Tyuiu.SosninFM.Sprint3.Task0.V7/Program.cs
@@ -7,7 +7,37 @@ namespace Tyuiu.SosninFM.Sprint3.Task0.V7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Соснин Ф. М. | АСОиУб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Соснин Фёдор Михайлович | АСОиУб-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет, и печатает результат на экране                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double value = 0.75;
+            int startvalue = 1;
+            int endvalue = 20;
+
+            Console.WriteLine("Переменная Х = " + value);
+            Console.WriteLine("Старт ранга = " + startvalue);
+            Console.WriteLine("Конец Ранга = " + endvalue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма ввода = " + ds.GetSumSeries(value, startvalue, endvalue));
+            Console.ReadKey();
         }
     }
 }
