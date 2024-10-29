71ecd8f4838c3da6e62dbffe21a0fb9a66dbbb84
федор соснин <sosninfedor@gmail.com>
2024-10-29 18:16:22 +0500

    Task4

--- Tyuiu.SosninFM.Sprint3.Task4.V28/Program.cs
+++ Tyuiu.SosninFM.Sprint3.Task4.V28/Program.cs
@@ -8,7 +8,34 @@ namespace Tyuiu.SosninFM.Sprint3.Task4.V28
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Title = "Спринт #3 | Выполнил: Соснин Ф. М. | АСОиУб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнил: Соснин Фёдор Михайлович | АСОиУб-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу                                                      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine( "Старт шага = " + startValue);
+            Console.WriteLine( "Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма ряда = " + ds.Calculate(startValue, stopValue));
+            Console.ReadKey();
+
         }
     }
 }
